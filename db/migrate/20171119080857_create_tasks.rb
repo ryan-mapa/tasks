7f2d62b868dda1239b87c553fb3edae9595f64dd
ryan mapa <ryan.m.mapa@gmail.com>
2017-11-19 00:15:03 -0800

    Migrate Task model

--- db/migrate/20171119080857_create_tasks.rb
+++ db/migrate/20171119080857_create_tasks.rb
@@ -1,12 +1,16 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
-      t.text :content
+      t.text :content, null: false
       t.integer :parent_id
-      t.integer :author_id
-      t.boolean :completed
+      t.integer :author_id, null: false
+      t.boolean :completed, default: false
+      t.integer :children, array: true, default: []
 
       t.timestamps
     end
+
+      add_index :tasks, :parent_id
+      add_index :tasks, :author_id
   end
 end
